402ed54234119f9622532b13fed40524be3fe151
mim21 <mim21@users.noreply.github.com>
2026-05-20 17:51:51 +0300

    run.bat: force-add index.html despite gitignore for Pages deploy

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ if errorlevel 1 (
 start index.html
 
 echo Publishing to GitHub Pages...
-git -C "%~dp0" add index.html events.json pipeline.py
+git -C "%~dp0" add -f index.html
 git -C "%~dp0" commit -m "Update events" 2>nul
 git -C "%~dp0" push
 echo Done. Live at https://mim21.github.io/merhav-bari/
